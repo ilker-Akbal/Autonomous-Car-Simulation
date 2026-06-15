9eedef62a6560dbc6f96c54a89d4ebcfb907962c
İlkerAkbal <ilkerakbal1@gmail.com>
2026-06-15 14:35:04 +0300

    Clean project to minimal CARLA ROS2 mission skeleton

--- autonomous_driving/setup.py
+++ autonomous_driving/setup.py
@@ -1,38 +1,34 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from glob import glob
+import os
 
-package_name = "autonomous_driving"
+package_name = 'autonomous_driving'
 
 setup(
     name=package_name,
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    version='0.0.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/autonomous_driving"]),
-        ("share/autonomous_driving", ["package.xml"]),
-        (
-            "share/autonomous_driving/launch",
-            [
-                "launch/teknofest_carla_full.launch.py",
-            ],
-        ),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'missions'), glob('missions/*')),
     ],
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="ilker",
-    maintainer_email="ilker.akbal4822@gop.edu.tr",
-    description="TEKNOFEST Robotaksi CARLA simulation and mission package",
-    license="MIT",
+    maintainer='ilker',
+    maintainer_email='ilker@example.com',
+    description='Minimal CARLA ROS2 skeleton for TEKNOFEST simulation.',
+    license='TODO',
+    tests_require=['pytest'],
     entry_points={
-        "console_scripts": [
-            "carla_world_manager_node = ros2_nodes.carla_world_manager_node:main",
-            "carla_sensor_bridge_node = ros2_nodes.carla_sensor_bridge_node:main",
-            "teknofest_mission_node = teknofest_sim.teknofest_mission_node:main",
-            "clean_lane_vision_node = ros2_nodes.clean_lane_vision_node:main",
-            "clean_phase1_driver_node = ros2_nodes.clean_phase1_driver_node:main",
-            "clean_carla_control_node = ros2_nodes.clean_carla_control_node:main",
-            "carla_spectator_follow_node = teknofest_sim.carla_spectator_follow_node:main",
-            "teknofest_spectator_follow_node = teknofest_sim.teknofest_spectator_follow_node:main",
+        'console_scripts': [
+            'carla_sensor_bridge_node = ros2_nodes.carla_sensor_bridge_node:main',
+            'carla_world_manager_node = ros2_nodes.carla_world_manager_node:main',
+            'teknofest_mission_node = teknofest_sim.teknofest_mission_node:main',
+            'carla_spectator_follow_node = teknofest_sim.carla_spectator_follow_node:main',
+            'teknofest_spectator_follow_node = teknofest_sim.teknofest_spectator_follow_node:main',
         ],
     },
 )
